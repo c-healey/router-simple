d1e1e630a4c19dff42c10c6f110a0403415deddd
Cathy <cathy.healey@gmail.com>
2020-05-03 20:53:23 -0400

    Updates

---
+++ precache-manifest.b346e1bd3ade26d0fd8976e76826116b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11a9fee10cb63c1774bb68381028c96",
+    "url": "/router-simple/index.html"
+  },
+  {
+    "revision": "fe2df6ee51bac09b64b1",
+    "url": "/router-simple/static/css/main.ed246ef6.chunk.css"
+  },
+  {
+    "revision": "a802ed75e7a42fb43255",
+    "url": "/router-simple/static/js/2.f2316ab3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/router-simple/static/js/2.f2316ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2df6ee51bac09b64b1",
+    "url": "/router-simple/static/js/main.3dc5e663.chunk.js"
+  },
+  {
+    "revision": "757910f96136a40a11dd",
+    "url": "/router-simple/static/js/runtime-main.35843cde.js"
+  }
+]);
